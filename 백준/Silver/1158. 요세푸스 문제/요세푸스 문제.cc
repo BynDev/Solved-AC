c5f2f2dd2a1358f4e63b445cfa0bb24e3d1decd1
Jaehyun Byun <143696056+JaehyeonByun@users.noreply.github.com>
2025-01-31 04:20:13 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 44 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N,K; 
+    cin >> N >> K;
+
+    queue<int>q;
+    
+    for(int i = 1; i <= N; i++){
+        q.push(i);
+    }
+    
+    cout << "<";
+    while(!q.empty())
+    {
+        for(int i = 0; i < K - 1;i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+        cout << q.front();
+        q.pop();
+
+        if(!q.empty()) cout << ", ";
+    }
+    cout << ">\n";
+}
